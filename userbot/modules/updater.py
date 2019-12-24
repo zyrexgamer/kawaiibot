3e4ffb9e1310ddf502935c5b8f2eb00470b3dee9
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-12-24 20:22:41 +0530

    UPDATER - Hope this works.

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -130,33 +130,33 @@ async def upstream(ups):
             repo.__del__()
             return
         for app in heroku_applications:
-            if app.name == str(HEROKU_APPNAME):
+            if app.name == HEROKU_APPNAME:
                 heroku_app = app
                 break
-            if heroku_app is None:
-                await ups.edit(
-                    f'{txt}\n`Invalid Heroku credentials for updating userbot dyno.`'
-                )
-                repo.__del__()
-                return
-            await ups.edit('`[HEROKU MEMEZ]\
-                            \nUserbot dyno build in progress, please wait.`')
-            ups_rem.fetch(ac_br)
-            repo.git.reset("--hard", "FETCH_HEAD")
-            heroku_git_url = heroku_app.git_url.replace("https://", "https://api:" + HEROKU_APIKEY + "@")
-            if "heroku" in repo.remotes:
-                remote = repo.remote("heroku")
-                remote.set_url(heroku_git_url)
-            else:
-                remote = repo.create_remote("heroku", heroku_git_url)
-            try:
-                remote.push(refspec="HEAD:refs/heads/master", force=True)
-            except GitCommandError as error:
-                await ups.edit(f'{txt}\n`Here is the error log:\n{error}`')
-                repo.__del__()
-                return
-            await ups.edit('`Successfully Updated!\n'
-                           'Bot is restarting... Wait for a second!`')
+        if heroku_app is None:
+            await ups.edit(
+                f'{txt}\n`Invalid Heroku credentials for updating userbot dyno.`'
+            )
+            repo.__del__()
+            return
+        await ups.edit('`[HEROKU MEMEZ]\
+                        \nUserbot dyno build in progress, please wait.`')
+        ups_rem.fetch(ac_br)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        heroku_git_url = heroku_app.git_url.replace("https://", "https://api:" + HEROKU_APIKEY + "@")
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(heroku_git_url)
+        else:
+            remote = repo.create_remote("heroku", heroku_git_url)
+        try:
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
+        except GitCommandError as error:
+            await ups.edit(f'{txt}\n`Here is the error log:\n{error}`')
+            repo.__del__()
+            return
+        await ups.edit('`Successfully Updated!\n'
+                       'Bot is restarting... Wait for a second!`')
     else:
         # Classic Updater, pretty straightforward.
         try:
